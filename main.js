c024f6618e61a4bafc42d4b9cd40905b41e545ae
HaroldF415 <harold.f415@gmail.com>
2022-12-13 21:01:42 -0500

    added helpful comments to remember

--- main.js
+++ main.js
@@ -7,3 +7,39 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+// grab all the divs of the 'color' class
+const colorPalleteDivPicked = document.querySelectorAll(".color");
+
+// add eventListener if they are 'clicked' for each one of those divs of the 'color' class
+colorPalleteDivPicked.forEach( colorDiv => { 
+
+  colorDiv.addEventListener('click', handleClick = () => { 
+
+    // access the 'background' property of the div clicked. This will get the 'color'
+    const color = colorDiv.style.background;
+
+    // access the '#current-color' div.
+    const currColor = document.getElementById("current-color");
+
+    // set the 'background' color of the 'current-color' background to that of the 'background' color of the clicked '.color' div
+    currColor.style.background = color;
+
+  }); 
+
+});
+
+// get all the divs created by the forLoop above.
+const cellPicked = document.querySelectorAll("#canvas .cell");
+
+// add a 'clicked' eventListener forEach of the divs clicked within the canvas.
+cellPicked.forEach( cell => {
+
+  cell.addEventListener('click', handleClick = () => {
+
+    // access the 'background' property of the cell
+    cell.style.background = document.getElementById("current-color").style.background;
+
+  });
+
+});
